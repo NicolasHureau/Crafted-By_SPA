4e28c1a9b7c0601940be04622eebee7d739ca0a0
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2024-03-25 16:49:47 +0100

    CartModal

--- src/components/NavbarIcons.vue
+++ src/components/NavbarIcons.vue
@@ -1,49 +1,63 @@
 <script>
 import UserRegisterModal from '@/components/UserRegisterModal.vue'
 import UserLoginModal from '@/components/UserLoginModal.vue'
+import CartModal from '@/components/icons/CartModal.vue'
 
 export default {
-  components: { UserLoginModal, UserRegisterModal },
+  components: { CartModal, UserLoginModal, UserRegisterModal },
   data: () => {
     return {
-      dropdownCart: [],
-      dropdownTotal: 0,
+      cart: {
+        products: [],
+        total: 0
+      }
     }
   },
   methods: {
     async updateDropdownCart() {
-      this.dropdownCart = [];
-      this.dropdownTotal= 0;
-      await this.$Cart.currentCart.forEach((product) => {
-        let dropdownProduct = this.$Product.getNameAndPrice(product[0])
-        dropdownProduct.push(this.$Cart.getProductCount(product[0]))
-        this.dropdownCart.push(dropdownProduct);
-        this.dropdownTotal += dropdownProduct[2]*dropdownProduct[3];
-      })
+
+      this.cart.products = [];
+      this.cart.total = 0;
+
+      if (this.$Cart.getCartCount > 0) {
+
+        await this.$Cart.currentCart.forEach((product) => {
+
+          let cartProduct = this.$Product.getNameAndPrice(product[0])
+          cartProduct.push(this.$Cart.getProductCount(product[0]))
+          this.cart.products.push(cartProduct);
+          this.cart.total += cartProduct[2]*cartProduct[3];
+        })
+      }
+      document.getElementById("CartModal").showModal();
     },
     openUserRegisterModal() {
+
       document.getElementById("UserLoginModal").close();
       document.getElementById("UserRegisterModal").showModal();
     },
     openUserLoginModal() {
+
       document.getElementById("UserRegisterModal").close();
       document.getElementById("UserLoginModal").showModal();
-    }
+    },
   }
 }
 </script>
 
 <template>
+
   <div class="inline-flex">
+
     <div class="flex items-center">
       <input type="search" class="" placeholder="Chercher">
       <i-ph-magnifying-glass class="icon mx-2" />
-
     </div>
 
     <div v-if="$User.token" class="flex items-center">
       <button role="button" class="dropdown">
         <i-ph-user-circle class="icon mx-2" />
+
         <div class="dropdown-content flex flex-col text-start rounded p-3 bg-base-100 shadow">
           <span class="text-secondary">Hello</span>
           <RouterLink to="/user/profile" class="inline-flex items-center">
@@ -59,12 +73,11 @@ export default {
             <i-ph-sign-out class="mr-1" />Déconnexion
           </a>
         </div>
+
       </button>
     </div>
+
     <div v-else class="flex items-center">
-<!--      <RouterLink to="/login">-->
-<!--        <i-ph-user-circle class="icon mx-2" />-->
-<!--      </RouterLink>-->
       <button @click="openUserRegisterModal">
         <i-ph-user-circle class="icon mx-2" />
       </button>
@@ -72,35 +85,20 @@ export default {
       <UserLoginModal />
     </div>
 
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" @click="updateDropdownCart" class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <i-ph-shopping-bag class="icon mx-2" />
-          <span class="badge badge-sm indicator-item bg-primary text-info">{{ $Cart.getCartCount }}</span>
-        </div>
+    <div tabindex="0" role="button" @click="updateDropdownCart" class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <i-ph-shopping-bag class="icon mx-2" />
+        <span class="badge badge-sm indicator-item bg-primary text-info">{{ $Cart.getCartCount }}</span>
       </div>
-      <div v-if="dropdownTotal > 0">
-        <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-          <div class="card-body">
-            <div v-for="product in dropdownCart" :key="product[0]" class="flex justify-between">
-              <span>{{ product[1] }}</span>
-              <div>
-<!--                single product price -->
-                <span>{{ product[2] }}</span>
-                <span> x {{ product[3] }}</span>
-              </div>
-            </div>
-          </div>
-          <span class="text-primary text-end">Total : {{ dropdownTotal.toFixed(2) }}</span>
-          <div class="card-actions">
-            <RouterLink to="/cart" type="button" class="btn btn-primary btn-block">Voir le panier</RouterLink>
-          </div>
-        </div>
-      </div>
+      <CartModal :cart=cart />
     </div>
+
   </div>
+
 </template>
+
 <style scoped>
+
 .icon {
   font-size: 25px;
   @media (max-width: 640px) {
@@ -112,4 +110,5 @@ input {
   position: absolute;
   width: 200px;
 }
+
 </style>
--- src/components/icons/CartModal.vue
+++ src/components/icons/CartModal.vue
@@ -1,13 +1,55 @@
 <script>
+
 export default {
-  name: 'CartModal'
+  name: 'CartModal',
+  props: {
+    cart: Object,
+  },
 }
 </script>
 
 <template>
 
+  <dialog id="CartModal" class="modal">
+
+    <div class="modal-box absolute right-0 rounded h-full w-1/3">
+
+      <div v-if="cart.total > 0">
+
+        <div v-for="product in cart.products" :key="product[0]" class="flex justify-between">
+          <span>{{ product[1] }}</span>
+          <div>
+            <span>{{ product[2] }}</span>
+            <span> x {{ product[3] }}</span>
+          </div>
+        </div>
+
+        <span class="text-primary text-end">Total : {{ cart.total.toFixed(2) }}</span>
+
+        <div>
+          <RouterLink to="/cart" type="button" class="btn btn-primary btn-block">Voir le panier</RouterLink>
+        </div>
+
+      </div>
+
+      <div v-else class="flex items-center justify-center h-full">
+        <p class="font-bold text-2xl">Votre panier est vide.</p>
+      </div>
+
+    </div>
+
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+
+  </dialog>
+
 </template>
 
 <style scoped>
+
+.modal-box {
+  max-height: 100vh;
+}
 
 </style>
